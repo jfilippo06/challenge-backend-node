5c12cad028b84b8d74a3c76d85aa15a51d42fc39
Jfilippo06 <jfilippo04@gmail.com>
2022-05-06 16:25:46 +0200

    routes

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require('dotenv').config()
+
+const app = express()
+
+app.use('/auth', require('./routes/auth'))
+app.use('/characters', require('./routes/characters'))
+app.use('/movies', require('./routes/movies'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, (req,res) => {
+    console.log(`listening in port ${PORT}`)
+})
