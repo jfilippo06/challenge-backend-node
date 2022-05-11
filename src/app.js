9fbcc754756339bdd6dc2d90c4f69a1e87358218
Jfilippo06 <jfilippo04@gmail.com>
2022-05-11 23:56:26 +0200

    routes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 app.use('/auth', require('./routes/auth'))
-// app.use('/characters')
-// app.use('/movies')
+app.use('/characters', require('./routes/characters'))
+app.use('/movies', require('./routes/movies'))
 
 module.exports = app
