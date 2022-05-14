268708d5d732c7422caca0b44242b0482ee69fa9
Jfilippo06 <jfilippo04@gmail.com>
2022-05-14 22:01:49 +0200

    model Usuario

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const app = require('./app')
 require('dotenv').config()
+const {sequelize} = require('./models/index')
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, (req,res) => {
     console.log(`Server is listening on port ${PORT}`)
+    sequelize.authenticate()
+    .then(()=>console.log('Database is listening'))
 })
