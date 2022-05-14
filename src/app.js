63ec63ab582618f83dda14b656b19a49cbfb52ad
Jfilippo06 <jfilippo04@gmail.com>
2022-05-14 20:15:50 +0200

    sequelize-cli

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 app.use('/auth', require('./routes/auth'))
 app.use('/characters', require('./routes/characters'))
 app.use('/movies', require('./routes/movies'))
