2599fbc5dbb82108254ddd9793d28ca5c74ac880
Jfilippo06 <jfilippo04@gmail.com>
2022-05-15 23:15:41 +0200

    login

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -15,26 +15,59 @@ const register = async (req,res) => {
     const salt = await bcrypt.genSalt(Number.parseInt(auth.rounds))
     const hash = await bcrypt.hash(password, salt)
 
-    try {
-        const usuario = await Usuario.create({
-            name,
-            email,
-            password: hash,
-        }).then(user =>{
-            const token = jwt.sign({user:user}, auth.secret, {expiresIn:auth.expired})
-            res.json({
-                user:user,
-                token:token
-            })
+    await Usuario.create({
+        name,
+        email,
+        password: hash,
+    }).then(user =>{
+        const token = jwt.sign({user:user}, auth.secret, {expiresIn:auth.expired})
+        res.json({
+            user:user,
+            token:token
         })
-        .catch(error => {
-            res.status(500).json(error)
-        })
-    } catch (error) {
-        return res.json({"error":error})
+    })
+    .catch(error => {
+        res.status(500).json({'error':error})
+    })
+}
+
+const login = async (req,res) => {
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        return res.json({"error":errors.array()})
     }
+
+    const {email, password} = req.body
+
+    await Usuario.findOne({
+        where: {
+            email:email
+        }
+    })
+    .then(user => {
+        if(!user){
+            res.status(404).json({'error':'Usuario no encontrado'})
+        }
+        else{
+            if(bcrypt.compareSync(password, user.password)){
+                const token = jwt.sign({user:user}, auth.secret, {expiresIn:auth.expired})
+                res.json({
+                    user:user,
+                    token:token
+                })
+            }
+            else{
+                res.status(404).json({'error':'Password no coinsiden'})
+            }
+        }
+    })
+    .catch(error => {
+        res.status(500).json({'error':error})
+    })
 }
 
 module.exports = {
     register,
+    login,
 }
