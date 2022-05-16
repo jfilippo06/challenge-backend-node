2d53750dc4420835cb36418b72f3c91586622d6a
Jfilippo06 <jfilippo04@gmail.com>
2022-05-16 20:49:25 +0200

    models

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, (req,res) => {
     console.log(`Server is listening on port ${PORT}`)
     sequelize.authenticate()
-    // sequelize.sync()
+    sequelize.sync()
     .then(()=>console.log('Database is listening'))
 })
