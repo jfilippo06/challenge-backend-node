1ec5b7a1f91b1c2105e630e661e05691a257464a
Jfilippo06 <jfilippo04@gmail.com>
2022-06-02 21:58:43 +0200

    cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+const corsOptions = {
+    credential: true,
+    origin: '*',
+}
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
