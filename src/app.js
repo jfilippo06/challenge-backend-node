3449d46a1bfdc8f4d7afb46967749685dee4f8ca
Jfilippo06 <jfilippo04@gmail.com>
2022-06-03 00:16:01 +0200

    swagger

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const swaggerUI = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+const path = require('path')
+
+const swaggerSpec = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Challenge Nodejs',
+            version: '1.0.0',
+        },
+        servers: [
+            {
+                url:'http://localhost:8000'
+            }
+        ]
+    },
+    apis: [`${path.join(__dirname, './routes/*.js')}`],
+}
 
 const corsOptions = {
     credential: true,
@@ -10,6 +29,7 @@ const corsOptions = {
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)))
 
 app.use('/auth', require('./routes/authRoutes'))
 app.use('/characters', require('./routes/charactersRoutes'))
--- src/routes/charactersRoutes.js
+++ src/routes/charactersRoutes.js
@@ -4,6 +4,30 @@ const auth = require('../middlewares/authMiddleware')
 const charatersBody = require('../middlewares/charactersBodyMiddleware')
 const { obtenerPersonajes, crearPersonaje, editarPersonaje, eliminarPersonaje } = require('../controllers/charactersControllers')
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      personaje:
+ *          properties:
+ *              imagen:
+ *                  type: string
+ *              nombre:
+ *                  type: string
+ *              edad:
+ *                  type: number
+ *              peso:
+ *                  type: string
+ *              historia:
+ *                  type: string
+ *          required:
+ *              - imagen
+ *              - nombre
+ *              - edad
+ *              - peso
+ *              - historia
+ */
+
 router.get('/', auth, obtenerPersonajes)
 router.post('/', auth, charatersBody, crearPersonaje)
 router.put('/:id', auth, charatersBody, editarPersonaje)
